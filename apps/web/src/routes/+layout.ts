b756809b6087fac0f4e24cec3ffd41611c6de434
void <voidarchived@gmail.com>
2026-01-10 14:53:27 +0545

    change posthog config

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -1,14 +1,11 @@
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
-import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
 
 export const load = async () => {
 	if (browser) {
-		posthog.init(PUBLIC_POSTHOG_KEY, {
-			api_host: PUBLIC_POSTHOG_HOST,
-			capture_pageview: false,
-			capture_pageleave: false,
-			capture_exceptions: true // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+		posthog.init('phc_jeqQwskahiaP3wSMZH9WKRkS3Sj6373SAw4pJAWja9m', {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'identified_only'
 		});
 	}
 
